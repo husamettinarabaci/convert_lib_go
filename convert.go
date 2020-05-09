9375d23d6d06c5ff356f156f58add1e980c4c3cd
husamettinarabaci <harabaci88@gmail.com>
2020-05-09 21:07:50 +0300

    Change Public Func

--- convert.go
+++ convert.go
@@ -5,7 +5,7 @@ import (
 	"time"
 )
 
-func bytesToTime(b []byte) time.Time {
+func BytesToTime(b []byte) time.Time {
 	var nsec int64
 	for i := uint8(0); i < 8; i++ {
 		nsec += int64(b[i]) << ((7 - i) * 8)
@@ -13,7 +13,7 @@ func bytesToTime(b []byte) time.Time {
 	return time.Unix(nsec/1000000000, nsec%1000000000)
 }
 
-func timeToBytes(t time.Time) []byte {
+func TimeToBytes(t time.Time) []byte {
 	nsec := t.UnixNano()
 	b := make([]byte, 8)
 	for i := uint8(0); i < 8; i++ {
@@ -22,11 +22,11 @@ func timeToBytes(t time.Time) []byte {
 	return b
 }
 
-func bytesToInt(b []byte) int64 {
+func BytesToInt(b []byte) int64 {
 	return int64(binary.BigEndian.Uint64(b))
 }
 
-func intToBytes(tracker int64) []byte {
+func IntToBytes(tracker int64) []byte {
 	b := make([]byte, 8)
 	binary.BigEndian.PutUint64(b, uint64(tracker))
 	return b
